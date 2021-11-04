08033d3cd7c124fd0ad620d338a517a5ebea9608
sjpfeiffer <steven@darebait.com>
2021-11-04 14:13:40 -0700

    Add new features and clean up code a bit
    Added a signup order number to the beginning of each players name, as well as a letter to signify that they signed up as late or tentative.

--- main.py
+++ main.py
@@ -1,53 +1,43 @@
 import pandas as pd
 import pyperclip
 
-# Get the beginning part of the URL created. We'll add to the names and roles string later.
+# Store some static data as variables to make it easier to change later if need be
 urlPrefix = 'https://tbc.wowhead.com/raid-composition'
 names = ';'
 roles = '#0'
 
-# Each spec has a unique ASCII character assigned to it, we're storing those here for use later.
-# Druid
 balanceDruidChar = "x"
 feralDruidChar = 'v'
 restoDruidChar = 'w'
 
-# Rogue
 assassinationRogueChar = 'k'
 combatRogueChar = 'j'
 subRogueChar = 'm'
 
-# Hunter
 beastMasterHunterChar = 'C'
 marksHunterChar = 'F'
 survHunterChar = 'D'
 
-# Shaman
 eleShamanChar = 'r'
 enhShamanChar = 't'
 restoShamanChar = 's'
 
-# Mage
 arcaneMageChar = 'd'
 fireMageChar = 'b'
 frostMageChar = 'c'
 
-# Warlock
 affWarlockChar = 'z'
 demonWarlockChar = 'B'
 destroWarlockChar = 'y'
 
-# Paladin
 holyPaladinChar = 'H'
 protPaladinChar = 'J'
 retPaladinChar = 'G'
 
-# Warrior
 armsWarriorChar = 'f'
 furyWarriorChar = 'h'
 protWarriorChar = 'g'
 
-# Priest
 discPriestChar = 'n'
 holyPriestChar = 'p'
 shadowPriestChar = 'q'
@@ -64,86 +54,82 @@ sorted_df.to_csv('csv/raidhelper.csv', index=False)
 # read new csv file
 f = pd.read_csv('csv/raidhelper.csv', usecols=['Name','Role','Spec'])
 
-# iterate through csv and check each line for name, role, spec. Adds names and spec to URL generator. Each spec has a specific character used in
-# wowheads raid composition tool
+# iterate through csv and check each line for name, role, spec. Adds names and roles to URL generator.
 for i in range(f['Name'].size):
     name = f['Name'][i]
     role = f['Role'][i]
     spec = f['Spec'][i]
 
     if role != 'Tentative' and role != 'Absence' and role != 'Late':
-        names = names + name + ';'
-    if role == 'Tank':
-        if spec == 'Guardian':
-            roles = roles + feralDruidChar
-        if spec == 'Protection':
-            roles = roles + protWarriorChar
-        if spec == 'Protection1':
-            roles = roles + protPaladinChar
-    if role == 'Druid':
-        if spec == 'Balance':
-            roles = roles + balanceDruidChar
-        if spec == 'Feral':
-            roles = roles + feralDruidChar
-        if spec == 'Restoration':
-            roles = roles + restoDruidChar
-    if role == 'Rogue':
-        if spec == 'Assassination':
-            roles = roles + assassinationRogueChar
-        if spec == 'Combat':
-            roles = roles + combatRogueChar
-        if spec == "Subtlety":
-            roles = roles + subRogueChar
-    if role == 'Hunter':
-        if spec == 'Beastmastery':
-            roles = roles + beastMasterHunterChar
-        if spec == 'Marksman':
-            roles = roles + marksHunterChar
-        if spec == 'Survival':
-            roles = roles + survHunterChar
-    if role == 'Shaman':
-        if spec == 'Elemental':
-            roles = roles + eleShamanChar
-        if spec == 'Enhancement':
-            roles = roles + enhShamanChar
-        if spec == 'Restoration1':
-            roles = roles + restoShamanChar
-    if role == 'Mage':
-        if spec == 'Arcane':
-            roles = roles + arcaneMageChar
-        if spec == 'Fire':
-            roles = roles + fireMageChar
-        if spec == 'Frost':
-            roles = roles + frostMageChar
-    if role == 'Warlock':
-        if spec == 'Affliction':
-            roles = roles + affWarlockChar
-        if spec == 'Demonology':
-            roles = roles + demonWarlockChar
-        if spec == 'Destruction':
-            roles = roles + destroWarlockChar
-    if role == 'Paladin':
-        if spec == 'Holy1':
-            roles = roles + holyPaladinChar
-        if spec == 'Protection':
-            roles = roles + protPaladinChar
-        if spec == 'Retribution':
-            roles = roles + retPaladinChar
-    if role == 'Warrior':
-        if spec == 'Arms':
-            roles = roles + armsWarriorChar
-        if spec == 'Fury':
-            roles = roles + furyWarriorChar
-        if spec == 'Protection':
-            roles = roles + protWarriorChar
-    if role == 'Priest':
-        if spec == 'Discipline':
-            roles = roles + discPriestChar
-        if spec == 'Holy':
-            roles = roles + holyPriestChar
-        if spec == 'Shadow':
-            roles = roles + shadowPriestChar
-            
+        names = names + '(' + str(i+1) + ') ' + name + ';'
+    if role == 'Tentative':
+        names = names + '(' + str(i + 1) + ') ' + name + ' (T);'
+    if role == 'Late':
+        names = names + '(' + str(i + 1) + ') ' + name + ' (L);'
+
+    if spec == 'Balance':
+        roles = roles + balanceDruidChar
+    if spec == 'Feral' or spec == 'Guardian':
+        roles = roles + feralDruidChar
+    if spec == 'Restoration':
+        roles = roles + restoDruidChar
+
+    if spec == 'Assassination':
+        roles = roles + assassinationRogueChar
+    if spec == 'Combat':
+        roles = roles + combatRogueChar
+    if spec == "Subtlety":
+        roles = roles + subRogueChar
+
+    if spec == 'Beastmastery':
+        roles = roles + beastMasterHunterChar
+    if spec == 'Marksman':
+        roles = roles + marksHunterChar
+    if spec == 'Survival':
+        roles = roles + survHunterChar
+
+    if spec == 'Elemental':
+        roles = roles + eleShamanChar
+    if spec == 'Enhancement':
+        roles = roles + enhShamanChar
+    if spec == 'Restoration1':
+        roles = roles + restoShamanChar
+
+    if spec == 'Arcane':
+        roles = roles + arcaneMageChar
+    if spec == 'Fire':
+        roles = roles + fireMageChar
+    if spec == 'Frost':
+        roles = roles + frostMageChar
+
+    if spec == 'Affliction':
+        roles = roles + affWarlockChar
+    if spec == 'Demonology':
+        roles = roles + demonWarlockChar
+    if spec == 'Destruction':
+        roles = roles + destroWarlockChar
+
+    if spec == 'Holy1':
+        roles = roles + holyPaladinChar
+    if spec == 'Protection1':
+        roles = roles + protPaladinChar
+    if spec == 'Retribution':
+        roles = roles + retPaladinChar
+
+    if spec == 'Arms':
+        roles = roles + armsWarriorChar
+    if spec == 'Fury':
+        roles = roles + furyWarriorChar
+    if spec == 'Protection':
+        roles = roles + protWarriorChar
+
+    if spec == 'Discipline':
+        roles = roles + discPriestChar
+    if spec == 'Holy':
+        roles = roles + holyPriestChar
+    if spec == 'Shadow':
+        roles = roles + shadowPriestChar
+
 # Copy generated URL to clipboard
 pyperclip.copy(urlPrefix + roles + names)
 
